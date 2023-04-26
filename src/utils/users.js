405776545fee96912c118eebe298bc926f1f37f0
VazMF <vazfernandam@gmail.com>
2023-04-26 16:16:48 -0300

    feat: adding logic to insert user and room

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,26 +1,41 @@
 const users = []
+const database = require('../../config/config')
 
 const addUser = ({ id, username, room }) => {
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
     if (!username || !room) {
-        return {
-            error: 'Informe uma sala e um nome de usuário!'
-        }
+        return reject(new Error('Informe uma sala e um nome de usuário!'))
     }
 
-    const existingUser = users.find((user) => {
-        return user.room === room && user.username === username
-    })
+    const existingUser = users.find((user) => user.room === room && user.username === username)
 
     if (existingUser) {
-        return {
-            error: 'Esse nome já está sendo usado.'
-        }
+        return reject(new Error('Esse nome já está sendo usado.'))
     }
 
+    const userInsertQuery = 'INSERT INTO user (username, password) VALUES (?, ?)'
+    const userInsertValues = [username, '123']
+
+    const roomInsertQuery = 'INSERT INTO room (name) VALUES (?)'
+    const roomInsertValues = [room]
+
     const user = { id, username, room }
+
+    database.query(userInsertQuery, userInsertValues, (error, result) => {
+        if (error) {
+            return reject(error)
+        }
+    })
+
+    database.query(roomInsertQuery, roomInsertValues, (error, result) => {
+        if (error) {
+            return reject(error)
+        }
+    })
+
+    console.log(user);
     users.push(user)
     return { user }
 }
@@ -39,7 +54,7 @@ const getUser = (id) => {
 
 const getUsersInRoom = (room) => {
     return users.filter((user) => user.room === room)
-} 
+}
 
 module.exports = {
     addUser,
